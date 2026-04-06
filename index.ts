8417c123d016a84feff36b865c8a13e7a44e3536
Mateus12-cyv <mateuspinho130@gmail.com>
2026-04-05 20:25:38 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
 index.ts
+
+class Livro {
+  constructor(
+    public titulo: string,
+    public autor: string,
+    public isbn: string
+  ) {}
+}
+
+class Usuario {
+  constructor(
+    public nome: string,
+    public email: string
+  ) {}
+}
+
+console.log("Sistema funcionando");
